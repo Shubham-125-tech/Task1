afd0d1c1971952647af2a0e3e780481e5edebeec
Shubham kumar sinha <s82007317@gmail.com>
2025-03-03 15:43:34 +0530

    Question based on bitwise operator

---
+++ index5.py
@@ -0,0 +1,15 @@
+print("QUESTION 1.")
+a=5
+b=3
+print("Bitwise and operator=",a&b)
+print("QUESTION 2.")
+x=12
+y=10
+print("Bitwise or operator=",x|y)
+print("QUESTION 3.")
+num=7
+print("Bitwwise not operator=", ~num)
+print("QUESTION 4.")
+u=9
+v=4
+print(" Bitwise xor operator=",u^v)
