8f99a29b4d587ef8588684dd2aa3b7bb80961510
Shubham kumar sinha <s82007317@gmail.com>
2025-03-04 14:03:23 +0530

    second code

---
+++ 1.py
@@ -0,0 +1,18 @@
+print("QUESTION 1")
+list=[10,20,30,40]
+print( 25 not in list )
+print("QUESTION 2")
+s="Helloworld"
+print( 'o' in s )
+print("QUESTION 3")
+nums=[5,10,15,20]
+print(30 not in nums)
+print("QUESTION 4")
+tuple1=(1,2,3,4,5)
+print(3 in tuple1)
+print(6 not in tuple1)
+print("QUESTION 5")
+A={1,2,3,4,5}
+B={3,6,9}
+print(3 in A)
+print(6 in B)
