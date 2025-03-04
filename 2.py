83f5b8040ca077f28396baefe443c6d3ce0c9165
Shubham kumar sinha <s82007317@gmail.com>
2025-03-04 14:12:17 +0530

    third  code

---
+++ 2.py
@@ -0,0 +1,20 @@
+print("QUESTION 1.")
+a=100
+b=100
+print(a is b)
+print("QUESTION 2.")
+x=500
+y=500
+print(x is y)
+print("QUESTION 3.")
+num1=-10
+num2=-10
+print(num1 is num2)
+print("QUESTION 4.")
+c=50.0
+d=50
+print(c is not d)
+print("QUESTION 5.")
+p=256
+q=256
+print(p is q)
