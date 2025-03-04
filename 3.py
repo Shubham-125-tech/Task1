f99424dd4ccf3c1b76b530e23f14c13d0bb7d817
Shubham kumar sinha <s82007317@gmail.com>
2025-03-04 14:15:14 +0530

    fourth code

---
+++ 3.py
@@ -0,0 +1,18 @@
+print("QUESTION 1.")
+a=5
+print("left shift=",a<<2)
+print("QUESTION 2.")
+x=16
+print("Right shift=",x>>3)
+print("QUESTION 3.")
+num=12
+print("Left shift=",num<<1)
+print("Right shift=",num>>2)
+print("QUESTION 4.")
+z=7
+print("Left shift=",z<<3)
+print("Right shift=",z>>1)
+print("QUESTION 5.")
+y=20
+print("Right shift",y>>2)
+print("left shift=",y<<1)
